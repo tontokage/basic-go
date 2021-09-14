5a746e5123d3bf7cee0a34c32549fea232252db2
tontokage <tontaka0710@gmail.com>
2021-09-15 01:42:06 +0900

    1章練習問題11まで。12編集中

---
+++ ch01/ex11.go
@@ -0,0 +1,64 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 17.
+//!+
+
+// Fetchall fetches URLs in parallel and reports their times and sizes.
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+)
+
+const (
+	filename = "fetchall.txt"
+	prefix   = "http://"
+)
+
+func main() {
+	start := time.Now()
+	ch := make(chan string)
+	inputFile, _ := os.ReadFile(os.Args[1])
+	//outputFile, _ := os.Create(filename)
+	//for _, b := range inputFile {
+	for _, b := range inputFile {
+		url := string(b)
+		if !strings.HasPrefix(url, prefix) {
+			url = prefix + url
+		}
+		fmt.Println(url)
+		go fetch(url, ch) // start a goroutine
+	}
+	for range os.Args[1:] {
+		//fmt.Fprintln(file, <-ch)
+		fmt.Println(<-ch) // receive from channel ch
+	}
+	//fmt.Fprintf(file, "%.2fs elapsed\n", time.Since(start).Seconds())
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+}
+
+func fetch(url string, ch chan<- string) {
+	start := time.Now()
+	resp, err := http.Get(url)
+	if err != nil {
+		ch <- fmt.Sprint(err) // send to channel ch
+		return
+	}
+
+	nbytes, err := io.Copy(io.Discard, resp.Body)
+	resp.Body.Close() // don't leak resources
+	if err != nil {
+		ch <- fmt.Sprintf("while reading %s: %v", url, err)
+		return
+	}
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%.2fs  %7d  %s", secs, nbytes, url)
+}
+
+//!-
