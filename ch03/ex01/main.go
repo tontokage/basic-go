28c7a094e9436d8cd3965859b512388e0f176d8b
tontokage <tontaka0710@gmail.com>
2021-10-15 11:08:15 +0900

    update 3章練習問題1

--- ch03/ex01/main.go
+++ ch03/ex01/main.go
@@ -28,12 +28,19 @@ func main() {
 		"style='stroke: grey; fill: white; stroke-width: 0.7' "+
 		"width='%d' height='%d'>", width, height)
 	for i := 0; i < cells; i++ {
+	innerfor:
 		for j := 0; j < cells; j++ {
 			ax, ay := corner(i+1, j)
 			bx, by := corner(i, j)
 			cx, cy := corner(i, j+1)
 			dx, dy := corner(i+1, j+1)
-			fmt.Printf("<polygon points='%g,%g %g,%g %g,%g %g,%g' fill="#99f" />\n",
+
+			for _, val := range []float64{ax, ay, bx, by, cy, cy, dx, dy} {
+				if math.IsNaN(val) {
+					continue innerfor
+				}
+			}
+			fmt.Printf("<polygon points='%g,%g %g,%g %g,%g %g,%g'/>\n",
 				ax, ay, bx, by, cx, cy, dx, dy)
 		}
 	}
